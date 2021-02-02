f6b44acc37cbc71772d7c579989d9fb141d320f2
yeongwoojang <sbtmxhs@gmail.com>
2021-02-02 13:36:59 +0900

    02/02 ttt브랜치 테스트

--- wweProject/src/com/wwe/common/filter/EncodingFilter.java
+++ wweProject/src/com/wwe/common/filter/EncodingFilter.java
@@ -14,8 +14,6 @@ public class EncodingFilter implements Filter {
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-
 	}
 
 	@Override
@@ -40,33 +38,4 @@ public class EncodingFilter implements Filter {
 		// TODO Auto-generated method stub
 
 	}
-
 }
-=======
-	}
-
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		request.setCharacterEncoding("UTF-8");
-
-		// ServletResponse 객체에는 setHeader() 가 없기 때문에
-		// contentType을 지정할 수 있게 해주는 setContentType 메서드 사용
-		response.setContentType("text/html; charset=UTF-8");
-
-		// 같은 url-pattern 으로 여러개의 filter를 지정할 수 있다.
-		// 해당 filter 들은 filter-chain형태로 관리되며, web.xml에 등록한 순서대로 실행된다.
-		// 필터체인을 탐색해 다음 필터가 있다면 다음 필터의 doFilter 메서드를 호출
-		// 다음 필터가 없다면 servlet을 호출한다.
-		chain.doFilter(request, response);
-
-	}
-
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-
-	}
-
-}
->>>>>>> branch 'Lee' of https://github.com/yeongwoojang/WWE_PROJECT.git
